a536ffad96c6d659a005ef1a382847dc9e0d4a4d
Noah Corona <noahcorona231@gmail.com>
2019-02-19 12:39:18 -0800

    Docs: Added docstrings to tests.

--- src/test/test_filestream.py
+++ src/test/test_filestream.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 from .. import filestream as fs
 
 res = Path('res')
@@ -6,6 +7,7 @@ testfile = res / 'testdoc.txt'
 
 
 def test_get_data():
+    """Asserts the data read is expected size."""
     with testfile.open() as fp:
         data = fp.read(10)
 
@@ -13,5 +15,11 @@ def test_get_data():
 
 
 def test_get_files():
+    """Asserts all files are accounted for."""
     files = list(res.iterdir())
     assert files == fs.get_files(res)
+
+
+def test_get_frequencies():
+    """Asserts frequency data is formatted correctly."""
+    pass
