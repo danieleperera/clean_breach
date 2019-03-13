e1097d86e8892a6d6fe3ab1c3fb85b31a295e508
daniele <daniele.storage1@gmail.com>
2019-03-13 23:08:20 +0100

    working progressbar

--- src/__main__.py
+++ src/__main__.py
@@ -13,24 +13,6 @@ parser.add_argument('-s', '--size', type=int, help='Please insert the size of th
 args = parser.parse_args()
 
 files = filestream.get_files(MEDIA)
-
-db.setup()
-
-from . import filestream
-from . import MEDIA
-import argparse  # sys https://www.pythonforbeginners.com/system/python-sys-argv
-from . import dbhandler
-import os
-from tqdm import tqdm
-
-db = dbhandler.DbHandler()
-
-
-parser = argparse.ArgumentParser(description='data cleanner')
-parser.add_argument('-s', '--size', type=int, help='Please insert the size of the file that you want to read')
-args = parser.parse_args()
-
-files = filestream.get_files(MEDIA)
 print(type(files))
 db.setup()
 
